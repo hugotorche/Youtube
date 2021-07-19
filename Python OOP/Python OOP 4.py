033772dcf5485ee16f1c3c7a3805cc8868cb7848
hugototo <81092822+hugototo@users.noreply.github.com>
2021-07-19 19:34:12 +0200

    Add files via upload

---
+++ Python OOP/Python OOP 4.py
@@ -0,0 +1,71 @@
+# Python Object-Oriented Programming
+
+# Tutorial 4: Inheritance - Creating Subclasses
+
+class Employee:
+
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+
+class Developer(Employee):
+    raise_amt = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+
+dev_1 = Developer('Hugo', 'Torche', 50000, 'Python')
+dev_2 = Developer('Test', 'User', 60000, 'Java')
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+
+print(dev_1.email)
+print(dev_2.prog_lang)
+
+print(dev_1.pay)
+dev_1.apply_raise()
+print(dev_1.pay)
+
+print(mgr_1.email)
+mgr_1.add_emp(dev_2)
+mgr_1.remove_emp(dev_1)
+mgr_1.print_emps()
+
+print(isinstance(dev_1, Developer))
+print(issubclass(Manager, Employee))
