5e8e51e5686134bb16d112feeeac3ab060b1152d
hugotorche <81092822+hugotorche@users.noreply.github.com>
2021-09-05 19:11:45 +0200

    Add files via upload

---
+++ Python Tutorials/Modules/Random Module.py
@@ -0,0 +1,83 @@
+import random
+
+# Get a random value
+# Random method
+
+value_1 = random.random()
+
+print(value_1)
+
+# Get a random value between 1 and 10
+# Uniform method
+
+value_2 = random.uniform(1, 10)
+
+print(value_2)
+
+# Get a random integer value between 1 and 10
+# Randint method
+
+value_3 = random.randint(1, 10)
+
+print(value_3)
+
+# Get a random choice from a list
+# Choice method
+
+greetings = ['Hello', 'Hi', 'Hey', 'Howdy', 'Hola']
+
+value_4 = random.choice(greetings)
+
+print(value_4 + ', Hugo!')
+
+# Get a list of random values from a list
+# Choices method
+
+colors = ['Red', 'Black', 'Green', 'Blue', 'White']
+
+value_5 = random.choices(colors, weights=[50, 60, 70, 80, 90], k=7)
+
+print(value_5)
+
+# Get a random card from a deck list
+# Sample method
+
+deck = list(range(1, 53))
+hand = random.sample(deck, k=5)
+
+print(hand)
+
+# Get a random dataset for analysis
+# Module to create basic random data
+
+first_names = ['John', 'Jane', 'Corey', 'Travis', 'Dave', 'Kurt', 'Neil', 'Sam', 'Steve', 'Tom', 'James', 'Robert', 'Michael',
+               'Charles', 'Joe', 'Mary', 'Maggie', 'Nicole', 'Patricia', 'Linda', 'Barbara', 'Elizabeth', 'Laura', 'Jennifer', 'Maria']
+
+last_names = ['Smith', 'Doe', 'Jenkins', 'Robinson', 'Davis', 'Stuart', 'Jefferson', 'Jacobs', 'Wright', 'Patterson', 'Wilks', 'Arnold', 'Johnson',
+              'Williams', 'Jones', 'Brown', 'Davis', 'Miller', 'Wilson', 'Moore', 'Taylor', 'Anderson', 'Thomas', 'Jackson', 'White', 'Harris', 'Martin']
+
+street_names = ['Main', 'High', 'Pearl', 'Maple', 'Park',
+                'Oak', 'Pine', 'Cedar', 'Elm', 'Washington', 'Lake', 'Hill']
+
+fake_cities = ['Metropolis', 'Eerie', "King's Landing", 'Sunnydale', 'Bedrock', 'South Park', 'Atlantis', 'Mordor', 'Olympus', 'Dawnstar', 'Balmora', 'Gotham', 'Springfield',
+               'Quahog', 'Smalltown', 'Epicburg', 'Pythonville', 'Faketown', 'Westworld', 'Thundera', 'Vice City', 'Blackwater', 'Oldtown', 'Valyria', 'Winterfell', 'Braavos‎', 'Lakeview']
+
+states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS',
+          'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
+
+for num in range(1):
+    first = random.choice(first_names)
+    last = random.choice(last_names)
+
+    phone = f'{random.randint(100, 999)}-555-{random.randint(1000,9999)}'
+
+    street_num = random.randint(100, 999)
+    street = random.choice(street_names)
+    city = random.choice(fake_cities)
+    state = random.choice(states)
+    zip_code = random.randint(10000, 99999)
+    address = f'{street_num} {street} St., {city} {state} {zip_code}'
+
+    email = first.lower() + last.lower() + '@bogusemail.com'
+
+    print(f'{first} {last}\n{phone}\n{address}\n{email}\n')
